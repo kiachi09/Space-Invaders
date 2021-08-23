ba568c5259c882fac1ed1112f936dc8db68a83bc
Dushyant Rai <55766655+kiachi09@users.noreply.github.com>
2021-08-23 10:55:39 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,141 @@
+import pygame
+import os
+import time
+import random
+
+from lasers import Laser
+from lasers import collide
+from ships import Ship
+from players import Player
+from enemies import Enemy
+pygame.font.init()
+
+WIDTH, HEIGHT = 1200, 800 
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Invader")
+
+# Background
+BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
+   
+def main():
+    run = True
+    FPS = 144
+    level = 0
+    lives = 5
+    main_font = pygame.font.SysFont("calibri", 50)
+    lost_font = pygame.font.SysFont("calibri", 70)
+     
+    enemies = []
+    wave_length = 5
+    enemy_vel = 1
+    
+    laser_vel = 5
+     
+    player_vel = 5
+    
+    player = Player(300, 630) 
+     
+    clock = pygame.time.Clock()
+    
+    lost = False
+    lost_count = 0
+    
+    def redraw_window():
+        WIN.blit(BG, (0, 0))
+        # draw text 
+        fps_label = main_font.render(f"FPS: {FPS}", 1, (255, 0, 0))
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255, 0 , 0)) 
+        level_label = main_font.render(f"Level: {level}", 1, (255, 0 , 0)) 
+        
+        WIN.blit(lives_label, (10, 10))
+        WIN.blit(fps_label, (10, lives_label.get_height()))
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+        
+        for enemy in enemies:
+            enemy.draw(WIN)
+            
+        player.draw(WIN)
+        
+        if lost:
+            lost_label = lost_font.render("You Lost!!", 1, (255, 255, 255))
+            WIN.blit(lost_label, (WIDTH/2 - lost_label.get_width() / 2, 350))
+        pygame.display.update()
+    
+    while run:
+        clock.tick(FPS)
+        redraw_window()
+        
+        
+        if lives <= 0 or player.health <= 0:
+            lost = True
+            lost_count += 1
+            
+        if lost:
+            if lost_count > FPS * 3:
+                run = False
+            else:
+                continue
+        
+        if len(enemies) == 0:
+            level += 1
+            wave_length += 5
+            for i in range(wave_length):
+                enemy = Enemy(random.randrange(50, WIDTH - 100), random.randrange(-1000*level, -200), random.choice(["red", "blue", "green"]))      
+                enemies.append(enemy)
+                        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit()
+                
+        keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_ESCAPE]: # esc
+            quit()
+        
+        if keys[pygame.K_a] or keys[pygame.K_LEFT] and player.x - player_vel> 0: # left
+            player.x -= player_vel
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT] and player.x + player_vel + player.get_width()< WIDTH: # right
+            player.x += player_vel
+        if keys[pygame.K_w] or keys[pygame.K_UP] and player.y - player_vel > 0: # up
+            player.y -= player_vel
+        if keys[pygame.K_s] or keys[pygame.K_DOWN] and player.y + player_vel + player.get_height() + 10< HEIGHT: # down
+            player.y += player_vel
+        if keys[pygame.K_SPACE]:
+            player.shoot() 
+         
+         
+        for enemy in enemies[:]:
+            enemy.move(enemy_vel)
+            enemy.move_lasers(laser_vel, player)
+            
+            if random.randrange(0, 2*144) == 1:
+                enemy.shoot()
+                
+            if collide(enemy, player):
+                player.health -= 10
+                enemies.remove(enemy)
+            elif enemy.y + enemy.get_height() > HEIGHT:
+                lives -= 1
+                enemies.remove(enemy)    
+                
+        player.move_lasers(-laser_vel, enemies)                        
+
+
+def main_menu():
+    title_font = pygame.font.SysFont("calibri", 70)
+    run = True
+    while run:
+        WIN.blit(BG, (0, 0))
+        title_label = title_font.render("Press the mouse to begin ...", 1, (255, 255, 255))
+        WIN.blit(title_label, (WIDTH/2 - title_label.get_width() / 2, 350))
+        pygame.display.update()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or event.type == pygame.K_ESCAPE:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                main()
+            
+    pygame.quit()           
+      
+main_menu()
