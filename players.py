b2c851a450385321b017d0079e3443d3881390f8
Dushyant Rai <55766655+kiachi09@users.noreply.github.com>
2021-08-23 11:05:26 -0700

    Update players.py

--- players.py
+++ players.py
@@ -1,20 +1,23 @@
 import pygame
 import os
 from ships import Ship
+
 # Player player
 YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
 # Lasers
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
-WIDTH, HEIGHT = 1200, 800 
+WIDTH, HEIGHT = 1200, 800
+
+
 class Player(Ship):
-    def __init__(self, x, y, health = 100):
+    def __init__(self, x, y, health=100):
         super().__init__(x, y, health)
         self.ship_img = YELLOW_SPACE_SHIP
         self.laser_img = YELLOW_LASER
         self.mask = pygame.mask.from_surface(self.ship_img)
         self.max_health = health
-        
+
     def move_lasers(self, vel, objs):
         self.cooldown()
         for laser in self.lasers:
@@ -26,12 +29,30 @@ class Player(Ship):
                     if laser.collision(obj):
                         objs.remove(obj)
                         if laser in self.lasers:
-                             self.lasers.remove(laser)
-    
+                            self.lasers.remove(laser)
+
     def draw(self, window):
         super().draw(window)
         self.healthbar(window)
-    
+
     def healthbar(self, window):
-        pygame.draw.rect(window, (255, 0, 0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width(), 10))    
-        pygame.draw.rect(window, (0, 255, 0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width() *(self.health/self.max_health), 10))
+        pygame.draw.rect(
+            window,
+            (255, 0, 0),
+            (
+                self.x,
+                self.y + self.ship_img.get_height() + 10,
+                self.ship_img.get_width(),
+                10,
+            ),
+        )
+        pygame.draw.rect(
+            window,
+            (0, 255, 0),
+            (
+                self.x,
+                self.y + self.ship_img.get_height() + 10,
+                self.ship_img.get_width() * (self.health / self.max_health),
+                10,
+            ),
+        )
