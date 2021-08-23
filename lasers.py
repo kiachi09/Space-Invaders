1b7ef24b58aaf1e8beb6e3797d97dd56f82d0016
Dushyant Rai <55766655+kiachi09@users.noreply.github.com>
2021-08-23 11:03:54 -0700

    Update lasers.py

--- lasers.py
+++ lasers.py
@@ -1,9 +1,11 @@
 import pygame
 
+
 def collide(obj1, obj2):
     offset_x = obj2.x - obj1.x
     offset_y = obj2.y - obj1.y
-    return obj1.mask.overlap(obj2.mask , (offset_x, offset_y)) != None 
+    return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None
+
 
 class Laser:
     def __init__(self, x, y, img):
@@ -11,15 +13,15 @@ class Laser:
         self.y = y
         self.img = img
         self.mask = pygame.mask.from_surface(self.img)
-        
+
     def draw(self, window):
         window.blit(self.img, (self.x, self.y))
-        
+
     def move(self, vel):
-        self.y+= vel
-    
+        self.y += vel
+
     def off_screen(self, height):
-        return not (self.y<= height and self.y>= 0)
-    
+        return not (self.y <= height and self.y >= 0)
+
     def collision(self, obj):
         return collide(self, obj)
